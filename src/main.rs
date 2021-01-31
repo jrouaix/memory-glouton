a18dde89e94b41df496a2dce0252eeb4ab7522ac
jrx <jerome+git@rouaix.eu>
2021-01-31 20:44:56 +0100

    same test with actix is rock stable !

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,57 @@
 use std::time::Duration;
 
-use bastion::prelude::*;
+use actix::prelude::*;
 
-fn main() {
+#[actix_rt::main]
+async fn main() {
+    let start_time = std::time::SystemTime::now();
+    let expected_duration = Duration::from_secs(10 * 10 * 10);
+
+    loop {
+        // start new actor
+        let addr = MyActor { count: 10 }.start();
+        // send message and get future for result
+        let _ = addr.send(Ping(10)).await.unwrap(); // if I remove this line, I have a mem leak ... :-?
+
+        let now = std::time::SystemTime::now();
+        let duration = now.duration_since(start_time).unwrap();
+        if duration > expected_duration {
+            break;
+        }
+    }
+    System::current().stop();
+}
+
+struct MyActor {
+    count: usize,
+}
+
+impl Actor for MyActor {
+    type Context = Context<Self>;
+}
+
+#[derive(Message)]
+#[rtype(result = "usize")]
+struct Ping(usize);
+
+impl Handler<Ping> for MyActor {
+    type Result = usize;
+
+    fn handle(&mut self, msg: Ping, ctx: &mut Context<Self>) -> Self::Result {
+        self.count += msg.0;
+        // ctx.stop();
+        self.count
+    }
+}
+
+// impl Drop for MyActor {
+
+// }
+
+fn main_bastion() {
+    use bastion::prelude::*;
+    use std::time::Duration;
+
     Bastion::init();
     Bastion::start();
 
